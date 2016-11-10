6107cfbd985d30d34d203127ac9e6924fb40ea21
Yale Work <yalework@Yale-Works-MacBook-Pro.local>
2016-11-09 23:22:50 -0800

    Knight path finder can take args from command linein math grid format

--- w1d5/knights_travails/knight_path_finder.rb
+++ w1d5/knights_travails/knight_path_finder.rb
@@ -2,7 +2,12 @@ require 'set'
 
 class KPF
 	def initialize
-		@board = Array.new(8) {(1..8).to_a}
+		@board = Array.new(8) {Array.new(8, nil)}
+	end
+
+	def self.math_grid_indices_to_coding_grid_indices(pos)
+		x, y = pos.map{|num| num - 1}
+		[7 - y, x]
 	end
 
 	def adjacent_positions(pos)
@@ -29,7 +34,7 @@ class KPF
 	end
 
 	def valid?(pos)
-		pos.all?{|num| num.between?(1, 8)}
+		pos.all?{|num| num.between?(0, 7)}
 	end
 
 	def travail(start, finish)
@@ -39,7 +44,7 @@ class KPF
 		until q.empty?
 			current_pos = q.shift
 			seen << current_pos
-			all_positions(current_pos).each do |adj|
+			adjacent_positions(current_pos).each do |adj|
 				unless seen.include?(adj)
 					q << adj
 					family[adj] = current_pos
@@ -60,10 +65,32 @@ class KPF
 			adj = family[adj]
 		end
 
-		[start] + result.reverse
+		make_look_pretty([start] + result.reverse)
+	end
+
+	def make_look_pretty(path)
+		count = 1
+		path.each do |pos|
+			i, j = pos
+			@board[i][j] = count
+			count += 1
+		end
+
+		puts ' _ _ _ _ _ _ _ _'
+		@board.each do |row|
+			row.map!{|square| square ? "|#{square}" : '|_'}
+			puts row.join('') + '|'
+		end
+		puts
 	end
 
 end
 
 k = KPF.new
-p k.travail([0, 0], [2, 2])
+start = ARGV[0].split("").map(&:to_i)
+start = KPF.math_grid_indices_to_coding_grid_indices(start)
+finish = ARGV[1].split("").map(&:to_i)
+finish = KPF.math_grid_indices_to_coding_grid_indices(finish)
+start ||= [1, 1]
+finish ||= [8, 8]
+k.travail(start, finish)
